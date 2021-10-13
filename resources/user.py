84486a638eff39eed53dfb57458f7f535c530d41
Sam'an <samangriffiths47@gmail.com>
2021-10-13 09:27:48 -0500

    :bust_in_silhouette::moneybag: user resources update

--- resources/user.py
+++ resources/user.py
@@ -1,4 +1,4 @@
-from middleware import read_token, strip_token
+from middleware import id_check, read_token, strip_token
 from flask_restful import Resource
 from datetime import datetime
 from models.user import User
@@ -9,46 +9,46 @@ from models.db import db
 class Users(Resource):
     def get(self, id):
         token = strip_token(request)
-        if read_token(token)['data']:
+        if read_token(token):
             user = User.by_id(id)
+            if not user:
+                return 'User Not found', 404
             return user.json().pop("password_digest")
         else:
-            return read_token(token)['payload'][0], read_token(token)['payload'][1]
+            return "Unauthorized", 403
 
     def patch(self, id):
         token = strip_token(request)
-        if read_token(token)['data']:
-            data = request.get_json()
-            user = User.by_id(id)
-            for key in data.keys():
-                setattr(user, key, data[key])
-            db.session.commit()
-            return user.json().pop("password_digest")
+        if read_token(token):
+            if id_check(request, User, id):
+                data = request.get_json()
+                user = User.by_id(id)
+                if not user:
+                    return 'User Not found', 404
+                for key in data.keys():
+                    setattr(user, key, data[key])
+                db.session.commit()
+                return user.json().pop("password_digest")
+            else:
+                return "Unauthorized", 403
         else:
-            return read_token(token)['payload'][0], read_token(token)['payload'][1]
+            return "Unauthorized", 403
 
     def delete(self, id):
         token = strip_token(request)
-        if read_token(token)['data']:
-            user = User.by_id(id)
-            if not user:
-                return {'msg': 'User Not found'}, 404
-            copy = {}
-            for key in user.json().pop("password_digest").keys():
-                copy[key] = user.json()[key]
-                copy['updated_at'] = str(datetime.utcnow())
-            db.session.delete(user)
-            db.session.commit()
-            return {'Deletion Successful', copy}
+        if read_token(token):
+            if id_check(request, User, id):
+                user = User.by_id(id)
+                if not user:
+                    return 'User Not found', 404
+                copy = {}
+                for key in user.json().pop("password_digest").keys():
+                    copy[key] = user.json()[key]
+                    copy['updated_at'] = str(datetime.utcnow())
+                db.session.delete(user)
+                db.session.commit()
+                return {'Deletion Successful', copy}
+            else:
+                return "Unauthorized", 403
         else:
-            return read_token(token)['payload'][0], read_token(token)['payload'][1]
-
-
-class AllUsers(Resource):
-    def get(self):
-        token = strip_token(request)
-        if read_token(token)['data']:
-            users = User.find_all()
-            return [user.json().pop("password_digest") for user in users]
-        else:
-            return read_token(token)['payload'][0], read_token(token)['payload'][1]
+            return "Unauthorized", 403
