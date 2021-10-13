161431e410353de3f2abcb0e33521db4beb34948
Sam'an <samangriffiths47@gmail.com>
2021-10-13 09:41:55 -0500

    :house::moneybag: shelter resources update

--- resources/shelter.py
+++ resources/shelter.py
@@ -1,7 +1,7 @@
-from middleware import read_token, strip_token
+from middleware import admin_check, id_check, read_token, strip_token
 from flask_restful import Resource
-from datetime import datetime
 from models.shelter import Shelter
+from datetime import datetime
 from flask import request
 from models.db import db
 from uuid import UUID
@@ -10,52 +10,64 @@ from uuid import UUID
 class Shelters(Resource):
     def get(self, id):
         token = strip_token(request)
-        if read_token(token)['data']:
-            id = UUID(id)
-            shelter = Shelter.by_id(id)
-            return shelter.json().pop("password_digest")
-        else:
-            return read_token(token)['payload'][0], read_token(token)['payload'][1]
-
-    def patch(self, id):
-        token = strip_token(request)
-        if read_token(token)['data']:
-            data = request.get_json()
-            id = UUID(id)
-            shelter = Shelter.by_id(id)
-            for key in data.keys():
-                setattr(shelter, key, data[key])
-            db.session.commit()
-            return shelter.json().pop("password_digest")
-        else:
-            return read_token(token)['payload'][0], read_token(token)['payload'][1]
-
-    def delete(self, id):
-        token = strip_token(request)
-        if read_token(token)['data']:
+        if read_token(token):
             id = UUID(id)
             shelter = Shelter.by_id(id)
             if not shelter:
-                return {'msg': 'shelter Not found'}, 404
-            copy = {}
-            for key in shelter.json().pop("password_digest").keys():
-                copy[key] = shelter.json()[key]
-                copy['updated_at'] = str(datetime.utcnow())
-                copy
-            db.session.delete(shelter)
-            db.session.commit()
-            return {'Deletion Successful', copy}
+                return 'Shelter Not found', 404
+            return shelter.json().pop("password_digest")
         else:
-            return read_token(token)['payload'][0], read_token(token)['payload'][1]
+            return "Unauthorized", 403
+
+    def patch(self, id):
+        token = strip_token(request)
+        if read_token(token):
+            if id_check(request, Shelter, id) or admin_check(request):
+                data = request.get_json()
+                id = UUID(id)
+                shelter = Shelter.by_id(id)
+                if not shelter:
+                    return 'Shelter Not found', 404
+                for key in data.keys():
+                    setattr(shelter, key, data[key])
+                db.session.commit()
+                return shelter.json().pop("password_digest")
+            else:
+                return "Unauthorized", 403
+        else:
+            return "Unauthorized", 403
+
+    def delete(self, id):
+        token = strip_token(request)
+        if read_token(token):
+            if id_check(request, Shelter, id) or admin_check(request):
+                id = UUID(id)
+                shelter = Shelter.by_id(id)
+                if not shelter:
+                    return 'Shelter Not found', 404
+                copy = {}
+                for key in shelter.json().pop("password_digest").keys():
+                    copy[key] = shelter.json()[key]
+                    copy['updated_at'] = str(datetime.utcnow())
+                    copy
+                db.session.delete(shelter)
+                db.session.commit()
+                return 'Deletion Successful', copy
+            else:
+                return "Unauthorized", 403
+        else:
+            return "Unauthorized", 403
 
 
 class Allshelters(Resource):
     def get(self):
         token = strip_token(request)
-        if read_token(token)['data']:
+        if read_token(token):
             data = request.get_json()
             shelters = Shelter.by_proximity(
                 data["proximity"], data["coordinates"])
+            if shelters.length == 0:
+                return "No Shelters Within Proximity Limit"
             return shelters
         else:
-            return read_token(token)['payload'][0], read_token(token)['payload'][1]
+            return "Unauthorized", 403
