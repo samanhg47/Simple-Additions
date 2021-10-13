e1da21feb8060a290da4c9fbc7f2de31f5202bc5
Sam'an <samangriffiths47@gmail.com>
2021-10-12 19:58:10 -0500

    :closed_lock_with_key: auth setup

---
+++ resources/auth.py
@@ -0,0 +1,30 @@
+from flask_restful import Resource
+from flask import request
+from models.user import User
+from middleware import create_token, gen_password, compare_password
+
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User.find_by_user_name(data["user_name"])
+        if user:
+            if compare_password(data["password"], user.json()["password_digest"]):
+                token = create_token(
+                    {"id": str(user.id), "user_name": user.user_name})
+                return token, 200
+        else:
+            return 'Unauthorized', 404
+
+
+class Register(Resource):
+    def post(self):
+        data = request.get_json()
+        params = {
+            "user_name": data["user_name"],
+            "password_digest": gen_password(data["password"])
+        }
+        user = User(**params)
+        user.create()
+        print(user.json())
+        return user.json(), 201
