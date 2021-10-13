6cb0f3237af3d9803d7c84583efdf8122e8099ef
Sam'an <samangriffiths47@gmail.com>
2021-10-12 22:52:42 -0500

    :snake: app.py update

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask
-from flask_restful import Api
-from flask_cors import CORS
-from flask_migrate import Migrate
-from models.db import db
-from models.post import Post
-from models.image import Image
-from models.shelter import Shelter
-from models.user import User
-from models.comment import Comment
 from resources import shelter, image, post, user, auth, comment
+from models.shelter import Shelter
+from models.comment import Comment
+from flask_migrate import Migrate
+from models.image import Image
+from flask_restful import Api
+from models.user import User
+from models.post import Post
+from flask_cors import CORS
+from models.db import db
 
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/flask_assocs"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 db.init_app(app)
