28411a97450dae27cb4f84b7f11e0d31dce318ed
Sam'an <samangriffiths47@gmail.com>
2021-10-13 12:04:59 -0500

    :framed_picture: image model update

--- models/image.py
+++ models/image.py
@@ -49,12 +49,10 @@ class Image(db.Model):
 
     @classmethod
     def by_id(cls, id):
-        id = UUID(id)
         image = Image.query.filter_by(id=id).first()
         return image
 
     @classmethod
     def by_post(cls, post_id):
-        post_id = UUID(post_id)
         posts = Image.query.filter_by(post_id=post_id).all()
         return posts
