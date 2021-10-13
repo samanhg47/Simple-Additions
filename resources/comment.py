b75b3e849dc6937f08cbe846bc6ef62793225924
Sam'an <samangriffiths47@gmail.com>
2021-10-13 10:17:15 -0500

    :raising_hand_man::moneybag: comment resources update

--- resources/comment.py
+++ resources/comment.py
@@ -1,4 +1,4 @@
-from middleware import read_token, strip_token
+from middleware import admin_check, id_check, read_token, strip_token
 from flask_restful import Resource
 from models.comment import Comment
 from datetime import datetime
@@ -6,15 +6,18 @@ from flask import request
 from models.db import db
 from uuid import UUID
 
+from models.user import User
+from resources.post import Posts
+
 
 class AllComments(Resource):
-    def get(self):
-        token = strip_token(request)
-        if read_token(token)['data']:
-            comments = Comment.find_all()
-            return [comment.json() for comment in comments]
-        else:
-            return read_token(token)['payload'][0], read_token(token)['payload'][1]
+    # def get(self):
+    #     token = strip_token(request)
+    #     if read_token(token)['data']:
+    #         comments = Comment.find_all()
+    #         return [comment.json() for comment in comments]
+    #     else:
+    #         return "Unauthorized", 403
 
     def post(self):
         token = strip_token(request)
@@ -27,65 +30,80 @@ class AllComments(Resource):
             comment.create()
             return comment.json(), 201
         else:
-            return read_token(token)['payload'][0], read_token(token)['payload'][1]
+            return "Unauthorized", 403
 
 
 class Comments(Resource):
-    def get(self, id):
-        token = strip_token(request)
-        if read_token(token)['data']:
-            id = UUID(id)
-            comment = Comment.by_id(id)
-            return comment.json().pop("password_digest")
-        else:
-            return read_token(token)['payload'][0], read_token(token)['payload'][1]
+    #     def get(self, id):
+    #         token = strip_token(request)
+    #         if read_token(token)['data']:
+    #             id = UUID(id)
+    #             comment = Comment.by_id(id)
+    #             if not comment:
+    #                 return 'Comment Not Found', 404
+    #             return comment.json().pop("password_digest")
+    #         else:
+    #             return "Unauthorized", 403
 
     def patch(self, id):
         token = strip_token(request)
         if read_token(token)['data']:
-            data = request.get_json()
-            id = UUID(id)
-            comment = Comment.by_id(id)
-            for key in data.keys():
-                setattr(comment, key, data[key])
-            db.session.commit()
-            return comment.json()
+            if id_check(request, Comment, id) or admin_check(request):
+                data = request.get_json()
+                id = UUID(id)
+                comment = Comment.by_id(id)
+                if not comment:
+                    return 'Comment Not Found', 404
+                if not comment:
+                    return 'Comment Not Found', 404
+                for key in data.keys():
+                    setattr(comment, key, data[key])
+                db.session.commit()
+                return comment.json()
+            else:
+                return "Unauthorized", 403
         else:
-            return read_token(token)['payload'][0], read_token(token)['payload'][1]
+            return "Unauthorized", 403
 
     def delete(self, id):
         token = strip_token(request)
         if read_token(token)['data']:
-            id = UUID(id)
-            comment = Comment.by_id(id)
-            if not comment:
-                return {'msg': 'comment Not Found'}
-            copy = {}
-            for key in comment.json().keys():
-                copy[key] = comment.json()[key]
-                copy['updated_at'] = str(datetime.utcnow())
-            db.session.delete(comment)
-            db.session.commit()
-            return {'Deletion Successful': copy}
+            if id_check(request, Comment, id) or admin_check(request):
+                id = UUID(id)
+                comment = Comment.by_id(id)
+                if not comment:
+                    return 'Comment Not Found', 404
+                copy = {}
+                for key in comment.json().keys():
+                    copy[key] = comment.json()[key]
+                    copy['updated_at'] = str(datetime.utcnow())
+                db.session.delete(comment)
+                db.session.commit()
+                return 'Deletion Successful', copy
+            else:
+                return "Unauthorized", 403
         else:
-            return read_token(token)['payload'][0], read_token(token)['payload'][1]
+            return "Unauthorized", 403
 
 
-class PostComments(Resource):
-    def get(self, post_id):
-        token = strip_token(request)
-        if read_token(token)['data']:
-            comments = Comment.by_post(post_id)
-            return [comment.json() for comment in comments]
-        else:
-            return read_token(token)['payload'][0], read_token(token)['payload'][1]
+# class PostComments(Resource):
+#     def get(self, post_id):
+#         token = strip_token(request)
+#         if read_token(token)['data']:
+#             comments = Comment.by_post(post_id)
+#             return [comment.json() for comment in comments]
+#         else:
+#             return "Unauthorized", 403
 
 
 class UserComments(Resource):
     def get(self, user_id):
         token = strip_token(request)
         if read_token(token)['data']:
-            comments = Comment.by_user(user_id)
-            return [comment.json() for comment in comments]
+            if id_check(request, User, user_id) or admin_check(request):
+                comments = Comment.by_user(user_id)
+                return [comment.json() for comment in comments]
+            else:
+                return "Unauthorized", 403
         else:
-            return read_token(token)['payload'][0], read_token(token)['payload'][1]
+            return "Unauthorized", 403
