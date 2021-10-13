0e5672352e70ecb0a3482f4980ba6a18f6c8c366
Sam'an <samangriffiths47@gmail.com>
2021-10-12 22:38:30 -0500

    :raising_hand_man::moneybag: comment resources init

---
+++ resources/comment.py
@@ -0,0 +1,81 @@
+from middleware import read_token, strip_token
+from flask_restful import Resource
+from models.comment import Comment
+from datetime import datetime
+from flask import request
+from models.db import db
+from uuid import UUID
+
+
+class Allcomments(Resource):
+    def get(self):
+        token = strip_token(request)
+        if read_token(token)['data']:
+            comments = Comment.find_all()
+            return [comment.json() for comment in comments]
+        else:
+            return read_token(token)['payload'][0], read_token(token)['payload'][1]
+
+    def post(self):
+        token = strip_token(request)
+        if read_token(token)['data']:
+            data = request.get_json()
+            params = {}
+            for key in data.keys():
+                params[key] = data[key]
+            comment = Comment(**params)
+            comment.create()
+            return comment.json(), 201
+        else:
+            return read_token(token)['payload'][0], read_token(token)['payload'][1]
+
+
+class comments(Resource):
+    def get(self, id):
+        token = strip_token(request)
+        if read_token(token)['data']:
+            id = UUID(id)
+            comment = Comment.by_id(id)
+            return comment.json().pop("password_digest")
+        else:
+            return read_token(token)['payload'][0], read_token(token)['payload'][1]
+
+    def patch(self, id):
+        token = strip_token(request)
+        if read_token(token)['data']:
+            data = request.get_json()
+            id = UUID(id)
+            comment = Comment.by_id(id)
+            for key in data.keys():
+                setattr(comment, key, data[key])
+            db.session.commit()
+            return comment.json()
+        else:
+            return read_token(token)['payload'][0], read_token(token)['payload'][1]
+
+    def delete(self, id):
+        token = strip_token(request)
+        if read_token(token)['data']:
+            id = UUID(id)
+            comment = Comment.by_id(id)
+            if not comment:
+                return {'msg': 'comment Not Found'}
+            copy = {}
+            for key in comment.json().keys():
+                copy[key] = comment.json()[key]
+                copy['updated_at'] = str(datetime.utcnow())
+            db.session.delete(comment)
+            db.session.commit()
+            return {'Deletion Successful': copy}
+        else:
+            return read_token(token)['payload'][0], read_token(token)['payload'][1]
+
+
+class Postcomments(Resource):
+    def get(self, post_id):
+        token = strip_token(request)
+        if read_token(token)['data']:
+            comments = Comment.by_post_id(post_id)
+            return [comment.json() for comment in comments]
+        else:
+            return read_token(token)['payload'][0], read_token(token)['payload'][1]
